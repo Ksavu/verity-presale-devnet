de2cdc75d9bbd2ccff4984245dcfbe275de54f3b
Ksavu <nenadv36@gmail.com>
2025-12-27 21:36:48 +0100

    Config update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
