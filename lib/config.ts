e97392e476f51a6e522f44aec35127c8c11bea81
Ksavu <nenadv36@gmail.com>
2025-12-27 18:41:22 +0100

    Config update

--- lib/config.ts
+++ lib/config.ts
@@ -1,8 +1,8 @@
 export const CONFIG = {
   tokenName: "Verity",
   tokenSymbol: "VTY",
-  softcapUSD: 100_000,
-  presaleWallet: "TVOJ_WALLET_ADDRESS",
+  softcapUSD: 200_000,
+  presaleWallet: "7ALEjJAikbPcRcTRT6722UEa18tHLf5cnz72SABy5NUg",
   supportedTokens: ["SOL", "USDC", "USDT"],
   rpcUrl: "https://api.devnet.solana.com",
 };
